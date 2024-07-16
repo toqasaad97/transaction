3f188a1b956b4be4c7838e0e2651c47db29b05bf
Your Name <you@example.com>
2024-07-16 12:33:08 +0300

    Add asynchronous function to fetch data from JSON file

--- js/index.js
+++ js/index.js
@@ -1,2 +1,13 @@
 let data;
 
+async function dataFetch() {
+    try {
+        const response = await fetch("../data.json");
+        data = await response.json();
+        console.log("Fetched data:", data);
+        displays();
+        createGraph();
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
